c34007bdcbbaa6255d6fcd4118d207ab481c27eb
DanilMMM <115102963+DanilMMM@users.noreply.github.com>
2023-10-27 12:34:14 +0300

    c

--- bank/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿    {
-        Chetvbanke chetvbanke = new Chetvbanke();
-        chetvbanke.Otk(34234, "Мельников Данил Олегович", 55700);
-        chetvbanke.Out();
-        chetvbanke.Dob(23423);
-        chetvbanke.umen(34556);
-    }
